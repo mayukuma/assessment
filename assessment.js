22c6587ff1b5c923ad8359663a0e6bd75bf39e9f
ribbonpanda <53473652+mayukuma@users.noreply.github.com>
2020-07-07 19:51:27 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -21,11 +21,12 @@
       return;
     }
 
-    // 診断結果表示エリアの作成
-    removeAllChildren(resultDivided);
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivided.appendChild(header);
+    // result-areaに 診断結果表示エリアの作成、処理実行、pタグで診断結果を表示
+    removeAllChildren(resultDivided);//診断エリアの初期化
+    const header = document.createElement('h3');//h3タグに「診断結果」の文字列を設定
+    header.innerText = '診断結果';//h3タグに「診断結果」の文字列を設定
+    resultDivided.appendChild(paragraph);//result-areaにh３変数を代入
+
 
     const paragraph = document.createElement('p');
     const result = assessment(userName);
@@ -39,10 +40,13 @@
     + encodeURIComponent(result);
     anchor.setAttribute('href', hrefValue);
     anchor.className = 'twitter-hashtag-button';
-    anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
+    anchor.setAttribute('data-text',result);
+    anchor.innerText = 'Tweet #あなたのいいところ';
     tweetDivided.appendChild(anchor);
-
-    twttr.widgets.load();
+   //make scriptTag
+const script = document.createElement('script');
+script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+tweetDivided.appendChild(script);
   };
 
   userNameInput.onkeydown = (event) => {
@@ -85,19 +89,10 @@
 
     // 文字のコード番号の合計を回答の数で割って添字の数値を求める
     const index = sumOfCharCode % answers.length;
+    //診断結果
     let result = answers[index];
-
-    result = result.replace(/{userName}/g, userName);
-    return result;
+    result = result.replace(/{userName}/g, userName);//置換、変数を繰り返せるg、文字列
+    return result;//診断結果
   };
 
-  // テストコード
-  console.assert(
-    assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
-    '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
-  );
-  console.assert(
-    assessment('太郎') === assessment('太郎'),
-    '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
-   );
 })();
